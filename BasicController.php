4e1a69d7aba7529542f186f4d0983117a0f643ab
samipanzer <sami.ulla@panzertechnologies.net>
2015-04-06 22:53:57 +0530

    Create BasicController.php

---
+++ BasicController.php
@@ -0,0 +1,45 @@
+<?php 
+
+class BasicController extends Controller {
+
+
+    public function getIndex()
+    {
+    
+
+    return Response::json(array (
+    	             'title','Fifth Avenue Leasing and Rental | Car Leasing and Rental',
+    	             'users',User::select('name','email','phone','image')->where('type','=','admin')->first(),
+    	             'slides',Slide::all(),
+                     'vehicles',Vehicle::where('available','=','1')->where('reserved','=','0')->get()
+    	             ));
+
+
+    }
+
+
+    public function AddNewsletter() {
+
+    $data = Input::all();
+    $email = trim($data['emailadd']);
+    $token = trim($data['token']);
+
+    if($token==Session::token())
+    {
+
+    $check_email = Newsletter::where('email_addr','=',$email)->get();
+    if(sizeof($check_email) > 0) {
+        return Response::json(array('error'=>'Subscription with this email address already done'));
+    }
+
+    $newsletter = new Newsletter;
+    $newsletter->email_addr= $email;
+    $newsletter->save();
+
+    return Response::json(array('success'=>'Email Successfully Added to Newsletter', 'email'=>$newsletter->email_addr, 'id'=>$newsletter->id));
+    }
+
+    return Response::json(array('error'=>'Token Mismatch'));
+    }
+
+}
